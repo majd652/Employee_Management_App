e37cdb0e79a33e2889c307cdd93ea6e5d94ef84b
Majd <majdzain809@gmail.com>
2026-09-14 23:15:38 +0200

    feat: validation to the login form

---
+++ src/app/core/models/login.model.ts
@@ -0,0 +1,9 @@
+export class LoginModel{
+    userName: string;
+    password: string;
+    
+    constructor(){
+        this.userName = '';
+        this.password = '';
+    }
+}
--- src/app/pages/login/login.ts
+++ src/app/pages/login/login.ts
@@ -2,25 +2,40 @@ import { HttpClient } from '@angular/common/http';
 import { Component, inject } from '@angular/core';
 import { environment } from '../../../environments/environment.development';
 import { Router } from '@angular/router';
-import { FormsModule } from '@angular/forms';
+import { FormsModule,FormGroup, Validators } from '@angular/forms';
 import { tokenConstant } from '../../core/globalConstants/tokenConstant';
+import { EmployeeFormValidationPipe } from '../../core/pipes/employee-form-validation-pipe';
+import { FormBuilder,ReactiveFormsModule } from '@angular/forms';
+import { LoginModel } from '../../core/models/login.model';
 @Component({
-  imports: [FormsModule],
+  imports: [FormsModule, EmployeeFormValidationPipe,ReactiveFormsModule],
   selector: 'app-login',
   styleUrl: './login.css',
   templateUrl: './login.html',
 })
 export class Login {
   router = inject(Router);
-  login: any = {
-    userName: "",
-    password: ""
+  loginForm: FormGroup;
+  login: LoginModel = new LoginModel();
+  constructor(private http: HttpClient,private fb:FormBuilder) {
+   this.loginForm = this.fb.group({
+    userName: ['',Validators.required],
+    password: ['',Validators.required]
+   })
   }
-  constructor(private http: HttpClient) {
-
+  markFormGroupTouched(formGroup: FormGroup){
+    Object.keys(formGroup.controls).forEach(key =>{
+      const control = formGroup.get(key);
+      control?.markAsTouched()
+    })
   }
+  get f(){
+    return this.loginForm.controls
+  }
+ 
   onLogin() {
-    this.http.post(environment.API_URL + "login", this.login).subscribe({
+    if (this.loginForm.valid) {
+    this.http.post(environment.API_URL + "login", this.loginForm.value).subscribe({
       next: (res: any) => {
         if (res.result) {
           localStorage.setItem(tokenConstant.TOKEN_KEY, JSON.stringify(res.data));
@@ -29,9 +44,11 @@ export class Login {
           alert("Login failed");
           console.log(res.message);
         }
-      }
-
-
+      }  
     })
+    }else{
+      this.markFormGroupTouched(this.loginForm);
+      alert("Please fill all the fields");
+    }
   }
 }
